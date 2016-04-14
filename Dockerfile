5cc7579e1d1902e855c465663e41ec3728805d19
Matti Rita-Kasari <matti.rita-kasari@jubic.fi>
2016-04-14 21:17:44 +0300

    Move persistent data to /volume and link all to D8 folder.

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN apt-get update && apt-get dist-upgrade -y && \
 
 RUN curl -k https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz | tar zx -C /var/www/
 RUN mv /var/www/drupal-${DRUPAL_VERSION} /var/www/drupal
-RUN cp -rf /var/www/drupal/sites /tmp/
+RUN cp -rf /var/www/drupal/sites/default /tmp/
 
 # Composer for Sabre installation
 ENV COMPOSER_VERSION 1.0.0-alpha11
@@ -27,14 +27,19 @@ RUN cd /var/www/webdav && composer require sabre/dav ~3.1.0 && composer update s
 
 # Add configuration files
 ADD config/default.conf /workdir/default.conf
-RUN rm -rf /etc/nginx/conf.d/default.conf && ln -s /var/www/drupal/sites/conf/default.conf /etc/nginx/conf.d/default.conf
+RUN rm -rf /etc/nginx/conf.d/default.conf && ln -s /volume/conf/default.conf /etc/nginx/conf.d/default.conf
 ADD config/settings.php /workdir/settings.php
 ADD entrypoint.sh /workdir/entrypoint.sh
+
+RUN rm -rf /var/www/drupal/themes/ && rm -rf /var/www/drupal/modules/ && rm -rf /var/www/drupal/sites/default
+RUN ln -s /volume/themes/ /var/www/drupal/themes
+RUN ln -s /volume/modules/ /var/www/drupal/modules
+RUN ln -s /volume/default/ /var/www/drupal/sites/default
 
 RUN chown -R 104:0 /var/www && chmod -R g+rw /var/www && \
     chmod a+x /workdir/entrypoint.sh && chmod g+rw /workdir
 
-VOLUME ["/var/www/drupal/sites"]
+VOLUME ["/volume"]
 
 # Additional CA certificate bundle (Mozilla)
 ADD mailchimp-ca.sh /workdir/mailchimp-ca.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,33 +6,24 @@ export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
-if [ ! -d /var/www/drupal/sites/default ]; then
-  # Copy initial sites and configuration
-  cp -arf /tmp/sites/* /var/www/drupal/sites/
+if [ ! -d /volume/themes ]; then
+  mkdir -p /volume/themes
+fi
 
-  # Download modules
-  IFS=';' read -r -a modules <<< "$DRUPAL_MODULES"
-  for module in "${modules[@]}"
-  do
-    echo "Downloading module $module"
-    drush dl $module -y --destination=/var/www/drupal/sites/all/modules/
-  done
+if [ ! -d /volume/modules ]; then
+  mkdir -p /volume/modules
+fi
 
-  # Download themes
-  IFS=';' read -r -a themes <<< "$DRUPAL_THEMES"
-  for theme in "${themes[@]}"
-  do
-    echo "Downloading theme $theme"
-    drush dl $theme -y --destination=/var/www/drupal/sites/all/themes/
-  done
-
+if [ ! -d /volume/default ]; then
+  cp -rf /tmp/default/ /volume/
+  cp /volume/default/default.settings.php /volume/default/settings.php
 fi
 
 # Move Nginx configuration if does not exist
-if [ ! -f /var/www/drupal/sites/conf/default.conf ]; then
+if [ ! -f /volume/conf/default.conf ]; then
     # Move Nginx configuration to volume
-    mkdir -p /var/www/drupal/sites/conf/
-    mv /workdir/default.conf /var/www/drupal/sites/conf/default.conf
+    mkdir -p /volume/conf/
+    mv /workdir/default.conf /volume/conf/default.conf
 fi
 
 if [ ! -f /tmp/dav_auth ]; then
